c93c86e3fc371af0a033c93e9f0ef763776e5075
Xiaofei Wang <xiaofeiwang@Xiaofeis-MBP.hub>
2020-09-19 11:15:21 +1200

    added contact form

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React from 'react';
 import PageWrapper from './components/PageWrapper';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 // Pages
 import Home from './components/Pages/Home';
 import About from './components/Pages/About';
+import Contact from './components/Pages/Contact';
 
 function App() {
-  return (
-    <Router>
-      <PageWrapper>
-        <Route
-          exact={true}
-          path="/"
-          component={Home}
-        />
+	return (
+		<Router>
+			<PageWrapper>
+				<Route exact={true} path="/" component={Home} />
 
-        <Route
-          path="/about"
-          component={About}
-        />
-      </PageWrapper>
-    </Router>
-  );
+				<Route path="/about" component={About} />
+
+				<Route path="/contact" component={Contact} />
+			</PageWrapper>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/components/Pages/Contact.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import Field from '../Common/Field';
+
+const fields = {
+	sections: [
+		[
+			{
+				name: 'name',
+				elementName: 'input',
+				type: 'text',
+				placeholder: 'Your Name*',
+			},
+			{
+				name: 'email',
+				elementName: 'input',
+				type: 'email',
+				placeholder: 'Your Email*',
+			},
+			{
+				name: 'phone',
+				elementName: 'input',
+				type: 'text',
+				placeholder: 'Your Phone Number*',
+			},
+		],
+		[
+			{
+				name: 'message',
+				elementName: 'textarea',
+				type: 'text',
+				placeholder: 'Type Your Message*',
+			},
+		],
+	],
+};
+
+class Contact extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			name: '',
+			email: '',
+			phone: '',
+			message: '',
+		};
+	}
+
+	submitForm = (e) => {
+		alert('Form Submitted');
+	};
+
+	render() {
+		return (
+			<section className="page-section" id="contact">
+				<div className="container">
+					<div className="text-center">
+						<h2 className="section-heading text-uppercase">Contact Us</h2>
+						<h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+					</div>
+					<form id="contactForm" name="sentMessage" novalidate="novalidate" onSubmit={(e) => this.submitForm(e)}>
+						<div className="row align-items-stretch mb-5">
+							{fields.sections.map((section, sectionIndex) => {
+								console.log('Rendering section', sectionIndex, 'with', section);
+								return (
+									<div className="col-md-6" key={sectionIndex}>
+										{section.map((field, i) => {
+											return (
+												<Field
+													{...field}
+													key={i}
+													value={this.state[field.name]}
+													onChange={(e) => this.setState({ [field.name]: e.target.value })}
+												/>
+											);
+										})}
+									</div>
+								);
+							})}
+						</div>
+						<div className="text-center">
+							<div id="success"></div>
+							<button className="btn btn-primary btn-xl text-uppercase" id="sendMessageButton" type="submit">
+								Send Message
+							</button>
+						</div>
+					</form>
+				</div>
+			</section>
+		);
+	}
+}
+
+export default Contact;
